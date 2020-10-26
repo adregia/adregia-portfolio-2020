92906de3a2ada76080deb4615ec242669776d793
Ahmed <ahmed.dregia@toyota.com>
2020-10-26 08:42:35 -0700

    Tweaks header, about section for mobile

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,4 +1,5 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Dropdown } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
 
 import styles from 'styles/header.module.css';
 
@@ -7,13 +8,33 @@ export default function Header() {
     document.getElementById(key).scrollIntoView();
   };
 
+  const menu = (
+    <Menu
+      theme="dark"
+      selectedKeys={[]}
+      mode="horizontal"
+      className={styles.menu}
+      onClick={navigate}
+      style={{ backgroundColor: 'transparent' }}
+    >
+      <Menu.Item key="home">Home</Menu.Item>
+      <Menu.Item key="about">About</Menu.Item>
+      <Menu.Item key="experience">Experience</Menu.Item>
+      <Menu.Item key="projects">Projects</Menu.Item>
+      <Menu.Item key="contact">Contact</Menu.Item>
+    </Menu>
+  );
+
   return (
     <Layout.Header className={styles.header}>
+      <Dropdown overlay={menu} trigger={['click']} className={styles.dropdown}>
+        <MenuOutlined style={{ color: 'white' }} />
+      </Dropdown>
       <Menu
         theme="dark"
         selectedKeys={[]}
         mode="horizontal"
-        className={styles.menu}
+        className={[styles.menu, styles.horizontalMenu].join(' ')}
         onClick={navigate}
         style={{ backgroundColor: 'transparent' }}
       >
